691b6591f2e8cf45df1aad4a0ba628843345c607
Camryn Pearson <camrynpearson@gmail.com>
2018-11-27 16:24:05 -0500

    update

--- client/UpdateTodo.js
+++ client/UpdateTodo.js
@@ -1,9 +1,43 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import Axios from "axios";
+import { runInNewContext } from "vm";
+import TodoForm from "./TodoForm";
 
 export default class UpdateTodo extends Component {
-  render () {
+
+  constructor() {
+    super();
+    this.state = {
+      taskName: "",
+      assignee: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+    try {
+      const res = await Axios.put(`/api/todos/${this.props.id}`, {
+        taskName: event.target.taskName.value,
+        assignee: event.target.assignee.value
+      });
+      console.log("what res.data in handleSubmit", res.data);
+      this.props.addTodo(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  render() {
     return (
-      <form></form>
-    )
+      <TodoForm state={this.state} handleSubmit={this.handleSubmit} handleChange={this.handleChange} />
+    );
   }
 }
